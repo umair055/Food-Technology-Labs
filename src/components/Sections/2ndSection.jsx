b488cd219d891356724f4ef1b8ba315f58414c13
Umair Tariq <umair.tariq@focusteck.com>
2024-12-23 18:40:48 +0500

    Icons added on homepage

--- src/components/Sections/2ndSection.jsx
+++ src/components/Sections/2ndSection.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 // import './ServiceSection.css'; // Uncomment if you have a CSS file for styling
 
 // Import service icons
 // import serviceIcon1 from './images/images/service-icon-1.png'; // Adjusted path for icon 1
 // import serviceIcon2 from '../images/service-icon-2.png'; // Ensure this path is correct
 // import serviceIcon3 from '../images/service-icon-3.png'; // Ensure this path is correct
-
+import truckIcon from "./images/free-shipping.png";
+import cardIcon from "./images/credit-card.png";
+import helplineIcon from "./images/customer-support.png";
 const ServiceSection = () => {
   return (
     <section className="section service">
@@ -13,25 +15,42 @@ const ServiceSection = () => {
         <ul className="service-list">
           <li className="service-item">
             <div className="item-icon">
-              <img src='' width="40" height="40" loading="lazy" alt="Truck icon" />
+              <img
+                src={truckIcon}
+                width="40"
+                height="40"
+                loading="lazy"
+                alt="Truck icon"
+              />
             </div>
             <h3 className="h3 item-title">Free Shipping</h3>
           </li>
 
           <li className="service-item">
             <div className="item-icon">
-              <img src='' width="40" height="40" loading="lazy" alt="Payment card icon" />
+              <img
+                src={cardIcon}
+                width="40"
+                height="40"
+                loading="lazy"
+                alt="Payment card icon"
+              />
             </div>
             <h3 className="h3 item-title">Safe Payment</h3>
           </li>
 
           <li className="service-item">
             <div className="item-icon">
-              <img src='' width="40" height="40" loading="lazy" alt="Helpline icon" />
+              <img
+                src={helplineIcon}
+                width="40"
+                height="40"
+                loading="lazy"
+                alt="Helpline icon"
+              />
             </div>
             <h3 className="h3 item-title">24/7 Support</h3>
           </li>
-          
         </ul>
       </div>
     </section>
